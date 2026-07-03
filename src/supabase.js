9c8140c882bd749d093813b958965ea4ad867b65
sskarimmohiddin-cpu <ss.karimmohiddin@gmail.com>
2026-07-03 20:16:17 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ojxdhcytzmuykdxtcspy.supabase.co';
-const supabaseKey = 'sb_publishable_cRURRbWajlafZbnraHOHyQ_ybkj4Mxo';
+const supabaseUrl = 'https://isbxdvwctzzgbtdawdmo.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzYnhkdndjdHp6Z2J0ZGF3ZG1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNzg4MTEsImV4cCI6MjA5ODY1NDgxMX0.mJtUiQ8GBmvBeZhyyUVhFitc23QqcYLu3-1zb-VdQ-E';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
